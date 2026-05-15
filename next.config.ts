2add4356dfdd50f9e541a1a08186339d31e84582
MaulRai <muhammadraihanofc@gmail.com>
2026-05-15 18:50:23 +0700

    fix: remove redundant module.exports and streamline allowedDevOrigins configuration

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  
+  // @ts-ignore
+  allowedDevOrigins: ['192.168.1.40'],
   turbopack: {
     root: __dirname,
   },
 };
-
-module.exports = {
-  allowedDevOrigins: ['192.168.1.40'],
-}
 
 export default nextConfig;
